63c8243f70e445542f0af88a40363cf65bfb8d66
Tony Zaitoun <tony.z.1711@gmail.com>
2019-01-10 22:45:07 +0200

    added server-client logic

--- iex_example/app.js
+++ iex_example/app.js
@@ -1,78 +1,56 @@
-const SocketClient = require('socket.io-client')
+const server = require('http').createServer();
+const io = require('socket.io')(server);
+const spawn = require('threads').spawn;
 const express = require('express')
 const app = express()
+app.use(express.json())
 const port = 3000
 
 const WINDOW_SIZE = 3600000 // 1 hour in millisconds
-
-// create socket object
-const url = 'https://ws-api.iextrading.com/1.0/tops'
-
-const socket = SocketClient(url)
 
 /**
  * This is a js object which contains the current subscribed symbols and their expiration date.
  */
 var symbol_queue = {}
 
-socket.on('message', (data)=> { consume(data) })
 
-socket.on('connect', () => {
-    console.log("Connection created")
-    init_application()
+ io.listen(port + 1);
+
+// create socket object
+
+var arr = []
+
+const thread = spawn(function(input, done) {
+
+    const url = 'https://ws-api.iextrading.com/1.0/tops'
+
+    const SocketClient = require('socket.io-client')
+
+    const socket = SocketClient(url)
+    
+    socket.on('message', (data)=> {
+        let objc = JSON.parse(data)
+        done(objc); 
+     })
+    
+     socket.on('connect', () => {
+         console.log("Connection created")
+         socket.emit('subscribe','firehose')
+     })
+});
+
+thread
+.send({})
+.on('message', (response)=> {
+    // TODO: manipulate the response and boradcast it
+    io.sockets.emit('message', response);
 })
 
+app.post('/api/subscribe',(req,res)=>{
+    // subscribe
 
-/**
- * This function sets up all the express routes.
- * 
- * We do this in a function and not in a global scope so we can init the app only after the socket connection was established.
- */
-function init_application() {
-    app.post('/api/subscribe', function(req, res) {
-        var symbol = req.body.symbol;
-    });
-}
+    // response success
+    res.send({ code : 200, result: arr })
+})
 
-// set up routes
-
-
-/**
- * This function consumes a single data item received from the stream.
- * 
- * @param {*} item 
- */
-function consume(item) {
-
-}
-
-/**
- * Adds a new symbol to the queue.
- * 
- * @param {String} symbol 
- */
-function addToQueue(symbol) {
-    symbol_queue[symbol] = new Date().getTime() + WINDOW_SIZE;
-}
-
-/**
- * Checks if the symbol in the queue exists and has a valid timestamp.
- * 
- * @param {String} symbol 
- */
-function doesExistInQueue(symbol) {
-    var timeStamp = symbol_queue[symbol]
-    
-    if (timeStamp == undefined) {
-        // time stamp not found
-        return false
-    } else {
-        // check expiration time
-        if (timeStamp > new Date().getTime()){
-            return true
-        } else {
-            delete symbol_queue[symbol]
-            return false
-        }
-    }
-}
+app.listen(port);
---
+++ iex_example/client.js
@@ -0,0 +1,16 @@
+
+const SocketClient = require('socket.io-client')
+
+const url = 'http://localhost:3001'
+
+
+const socket = SocketClient(url)
+    
+socket.on('message', (data)=> {
+    console.log(data)
+ })
+
+ socket.on('connect', () => {
+     console.log("Connection created")
+     socket.emit('subscribe','firehose')
+ })
