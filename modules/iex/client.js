10603886acd63b134d30fdaf1e653c6dfa1b88de
Tony Zaitoun <tony.z.1711@gmail.com>
2019-01-15 01:45:31 +0200

    added twitter service

--- modules/iex/client.js
+++ modules/iex/client.js
@@ -1,14 +1,13 @@
-
 const SocketClient = require('socket.io-client')
 
 const url = 'http://localhost:9009'
-
-
 const socket = SocketClient(url)
+
+console.log("creating socket")
 
 socket.on('message', (data)=> {
     console.log(data)
- })
+})
 
  socket.on('connect', () => {
      console.log("Connection created")
